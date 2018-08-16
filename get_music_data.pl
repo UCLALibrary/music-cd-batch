8cf730aa66809cb24dc1bc3d9c208118d08d294b
Andy Kohler <akohler@library.ucla.edu>
2018-08-16 14:43:37 -0700

    VBT-1035: Use correct method when adding 005 to OCLC records...

--- get_music_data.pl
+++ get_music_data.pl
@@ -585,7 +585,7 @@ sub add_local_fields {
 
   # Create 005 timestamp for start of today
   my $f005_timestamp = '20' . get_yymmdd() . '000000.0';
-  $marc_record->append_fields(MARC::Field->new('005', $f005_timestamp));
+  $marc_record->insert_fields_ordered(MARC::Field->new('005', $f005_timestamp));
 
   # Add 049 field: 049 ## $a CLUV $o muclsdsdr $p {SPAC} $l {barcode} 
   my $fld049 = MARC::Field->new('049', ' ', ' ', 
