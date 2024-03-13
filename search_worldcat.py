60020dd01a23ecb3f3af0dec8aa09dbbfb33e710
Andy Kohler <akohler@library.ucla.edu>
2024-03-12 19:50:35 -0800

    Refactor search script, add OCLC holdings check to worldcat class

--- search_worldcat.py
+++ search_worldcat.py
@@ -1,14 +1,13 @@
 import argparse
 from csv import DictReader
-
-# from pymarc import Record
+from pymarc import Record
 from api_keys import (
     WORLDCAT_METADATA_CLIENT_ID,
     WORLDCAT_METADATA_CLIENT_SECRET,
     WORLDCAT_PRINCIPAL_ID,
     WORLDCAT_PRINCIPAL_IDNS,
 )
-from searchers.worldcat import Worldcat
+from searchers.worldcat import WorldcatClient
 
 # from pprint import pprint
 
@@ -20,27 +19,44 @@ def get_dicts_from_tsv(filepath=str) -> list:
     return full_dicts
 
 
+def get_worldcat_records(
+    worldcat_client: WorldcatClient, search_term: str, search_index: str
+) -> list[Record]:
+    search_results = worldcat_client.search_worldcat(search_term, search_index)
+    oclc_numbers = worldcat_client.get_oclc_numbers(search_results)
+    # TEMPORARY
+    for oclc_number in oclc_numbers:
+        held_by_clu = worldcat_client.is_held_by(oclc_number)
+        print(f"\t{oclc_number} held by CLU: {held_by_clu}")
+    marc_records = worldcat_client.get_worldcat_records(oclc_numbers)
+    return marc_records
+
+
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("music_data_file", help="Path to the TSV file of music data")
     args = parser.parse_args()
     music_data = get_dicts_from_tsv(args.music_data_file)
 
-    worldcat_client = Worldcat(
+    worldcat_client = WorldcatClient(
         WORLDCAT_METADATA_CLIENT_ID,
         WORLDCAT_METADATA_CLIENT_SECRET,
         WORLDCAT_PRINCIPAL_ID,
         WORLDCAT_PRINCIPAL_IDNS,
     )
 
-    # TODO: Remove range when done testing
-    for row in music_data[0:10]:
+    # TEMPORARY
+    print("TESTING TRUE: ", worldcat_client.is_held_by("28745774"))
+
+    # TODO: Remove range, used to test small subset of batch_016_20240229.tsv
+    for row in music_data[6:10]:
         upc_code = row["UPC"]
         official_title = row["title"]
-        oclc_numbers = worldcat_client.search_worldcat(upc_code, "sn")
         print(f"Searching for {upc_code} ({official_title})")
-        print(oclc_numbers)
-        marc_records = worldcat_client.get_worldcat_records(oclc_numbers)
+        # TODO: Error handling, probably best handled in main program
+        marc_records = get_worldcat_records(
+            worldcat_client=worldcat_client, search_term=upc_code, search_index="sn"
+        )
         for record in marc_records:
             print(f"\t{record.title}")
         print("")
--- searchers/worldcat.py
+++ searchers/worldcat.py
@@ -2,8 +2,16 @@ from io import BytesIO
 from bookops_worldcat import WorldcatAccessToken, MetadataSession
 from pymarc import parse_xml_to_array, Record
 
+# This uses bookops_worldcat, currently on version 0.5.0 which
+# does not yet fully support OCLC's Metadata API 2.0.
+# Some methods still use Metadata API 1.0, which is deprecated
+# and scheduled to be decommissioned 2024-04-30.
+# Devs are working on bookops_worldcat to bring it up to date;
+# there may be breaking changes from this work.
+# https://github.com/BookOps-CAT/bookops-worldcat/issues/63
 
-class Worldcat:
+
+class WorldcatClient:
     def __init__(
         self,
         key: str,
@@ -37,32 +45,36 @@ class Worldcat:
         otherwise, obtain and set new token.
         """
         if (self._token is not None) and (not self._token.is_expired()):
-            # Do nothing, return later
+            # Do nothing, return at end
             pass
         else:
             self._set_authentication_token()
         return self._token
 
-    def search_worldcat(self, search_term: str, search_index: str) -> list:
-        """Search Worldcat via Bookops implementation of OCLC's Metadata API /search/brief-bibs
-        Return list of OCLC numbers matching the search, or empty list if no records found.
+    def search_worldcat(self, search_term: str, search_index: str) -> dict:
+        """Search Worldcat via Bookops implementation of OCLC's Metadata API /search/brief-bibs.
+        Return dict with search results.
         """
         query = f"{search_index}:{search_term}"
         with MetadataSession(authorization=self.token) as session:
             response = session.search_brief_bibs(q=query)
             # TODO: Handle failures / errors
-            results = response.json()
-            if results.get("numberOfRecords") == 0:
-                oclc_numbers = []
-            else:
-                # Actual data is in list of dictionaries in briefRecords
-                oclc_numbers = [
-                    record.get("oclcNumber") for record in results.get("briefRecords")
-                ]
-            return oclc_numbers
+            return response.json()
+
+    def get_oclc_numbers(self, search_results: dict) -> list:
+        """Return list of OCLC numbers matching the search, or empty list if no records found."""
+        if search_results.get("numberOfRecords") == 0:
+            oclc_numbers = []
+        else:
+            # Actual data is in list of dictionaries in briefRecords
+            oclc_numbers = [
+                record.get("oclcNumber")
+                for record in search_results.get("briefRecords")
+            ]
+        return oclc_numbers
 
     def get_worldcat_records(self, oclc_numbers: list) -> list[Record]:
-        """Retrive full MARC records from Worldcat, using Bookops implementation of OCLC's
+        """Retrieve full MARC records from Worldcat, using Bookops implementation of OCLC's
         Metadata (1.0) API /bib/data and pymarc's XML -> MARC conversion.
         Return list of MARC records, or empty list if no records found.
         """
@@ -74,3 +86,12 @@ class Worldcat:
                 bib = parse_xml_to_array(data)[0]
                 records.append(bib)
         return records
+
+    def is_held_by(self, oclc_number: str, oclc_symbol: str = "CLU") -> bool:
+        """Determine whether the given OCLC number is held by the given OCLC symbol.
+        Defaults to CLU for UCLA.
+        """
+        with MetadataSession(authorization=self.token) as session:
+            response = session.holding_get_status(oclc_number, instSymbol=oclc_symbol)
+            data = response.json()
+            return data.get("content").get("holdingCurrentlySet")
