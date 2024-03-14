816ee10e4a4035a66805214f8afad269bb6fbde6
Andy Kohler <akohler@library.ucla.edu>
2024-03-14 14:44:17 -0800

    Refactor MusicBrainz code into class, with passing tests

---
+++ searchers/musicbrainz.py
@@ -0,0 +1,50 @@
+import musicbrainzngs
+
+
+class MusicbrainzClient:
+    """Provide a wrapper around specific functionality for musicbrainzngs
+    (https://pypi.org/project/python3-discogs-client/) to support
+    UCLA's batch music CD cataloging project.
+    """
+
+    def __init__(self) -> None:
+        # Client will be set on first use.
+        self._client = None
+
+    @property
+    def client(self) -> musicbrainzngs:
+        """Return configured client ready for use, on demand."""
+        if self._client is None:
+            # musicbrainzngs has no explicit "client" attribute like Discogs & Worldcat;
+            # create one to make our custom classes similar.
+            self._client = musicbrainzngs
+            self._client.set_useragent(
+                app="music-cd-batch",
+                version="0.1",
+                contact="https://github.com/UCLALibrary/music-cd-batch/",
+            )
+        return self._client
+
+    def search_musicbrainz_by_upc(self, upc: str) -> list:
+        """Search MusicBrainz for releases by UPC. Returns a list of release dictionaries.
+        To match both CDs and UPCs precisely, use strict=True
+        MusicBrainz calls UPCs "barcode"s.
+        """
+        result = self.client.search_releases(barcode=upc, format="CD", strict=True)
+        return result["release-list"]
+
+    def parse_musicbrainz_data(self, data: list) -> list:
+        """Parse MusicBrainz list of releases to pull out data for future use.
+        Each dictionary contains title, artist, publisher_number, and full_json of the
+        original response.
+        """
+        output_dict_list = []
+        for release in data:
+            release_dict = {
+                "title": release["title"],
+                "artist": release["artist-credit-phrase"],
+                "publisher_number": release["label-info-list"][0]["catalog-number"],
+                "full_json": release,
+            }
+            output_dict_list.append(release_dict)
+        return output_dict_list
--- tests/test_search_musicbrainz.py
+++ tests/test_search_musicbrainz.py
@@ -1,4 +1,4 @@
-from search_musicbrainz import parse_musicbrainz_data
+from searchers.musicbrainz import MusicbrainzClient
 import json
 import unittest
 
@@ -6,23 +6,31 @@ import unittest
 class TestSearchMusicbrainz(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
+        # Create client for testing; fake values, only for initialization
+        cls.musicbrainz_client = MusicbrainzClient()
         # Load sample data for use by all tests
         with open("tests/sample_data/musicbrainz_samples.data") as f:
             cls.data = json.load(f)
 
     def test_parse_musicbrainz_data_one_result(self):
-        result = parse_musicbrainz_data(self.data["018777260022"])
+        result = self.musicbrainz_client.parse_musicbrainz_data(
+            self.data["018777260022"]
+        )
         self.assertEqual(len(result), 1)
         self.assertEqual(result[0]["title"], "Lincoln")
         self.assertEqual(result[0]["artist"], "They Might Be Giants")
         self.assertEqual(result[0]["publisher_number"], "7 72600-2")
 
     def test_parse_musicbrainz_data_no_result(self):
-        result = parse_musicbrainz_data(self.data["ZZZZZZZZZZZZZ"])
+        result = self.musicbrainz_client.parse_musicbrainz_data(
+            self.data["ZZZZZZZZZZZZZ"]
+        )
         self.assertEqual(len(result), 0)
 
     def test_parse_musicbrainz_data_split_release(self):
-        result = parse_musicbrainz_data(self.data["020282009621"])
+        result = self.musicbrainz_client.parse_musicbrainz_data(
+            self.data["020282009621"]
+        )
         self.assertEqual(len(result), 1)
         self.assertEqual(result[0]["title"], "BYO Split Series, Volume V")
         # split release has two artists, and artist credit phrase is a concatenation of both
@@ -30,7 +38,9 @@ class TestSearchMusicbrainz(unittest.TestCase):
         self.assertEqual(result[0]["publisher_number"], "BYO 096")
 
     def test_parse_musicbrainz_data_multiple_results(self):
-        result = parse_musicbrainz_data(self.data["075596090728"])
+        result = self.musicbrainz_client.parse_musicbrainz_data(
+            self.data["075596090728"]
+        )
         self.assertEqual(len(result), 5)
         self.assertEqual(result[0]["title"], "Flood")
         self.assertEqual(result[0]["artist"], "They Might Be Giants")
