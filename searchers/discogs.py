69dbcd4c779bcf615cb0175723aaa0745837e65c
Andy Kohler <akohler@library.ucla.edu>
2024-03-14 14:21:29 -0800

    Refactor Discogs code into class, with passing tests

---
+++ searchers/discogs.py
@@ -0,0 +1,64 @@
+from discogs_client import Client
+
+
+class DiscogsClient:
+    """Provide a wrapper around specific functionality for discogs_client
+    (https://pypi.org/project/python3-discogs-client/) to support
+    UCLA's batch music CD cataloging project.
+    """
+
+    def __init__(self, user_token: str) -> None:
+        # user_token is required for many API requests.
+        self._token = user_token
+        # user_agent is defined locally, to identify our application.
+        self._user_agent = (
+            "music-cd-batch/0.1 +https://github.com/UCLALibrary/music-cd-batch"
+        )
+        # Client will be set on first use.
+        self._client = None
+
+    @property
+    def client(self) -> Client:
+        """Return configured Client ready for use, on demand."""
+        if self._client is None:
+            self._client = Client(user_agent=self._user_agent, user_token=self._token)
+        return self._client
+
+    def get_discogs_ids_by_upc(self, upc: str) -> list:
+        """Search Discogs for releases by UPC.
+        Returns a list of IDs to use to get full release data.
+        """
+        search_results = self.client.search(upc, type="release", format="CD")
+        release_ids = [result.id for result in search_results]
+        return release_ids
+
+    def get_full_discogs_releases(self, release_ids: list) -> list:
+        """Get full release data from Discogs by release ID."""
+        output_list = []
+        for release_id in release_ids:
+            release = self.client.release(release_id)
+            # force the release to refresh to get full data
+            release.refresh()
+            output_list.append(release.data)
+        return output_list
+
+    def parse_discogs_data(self, release_list: list) -> list:
+        """Parse Discogs list of releases to pull out data for future use.
+        Each dictionary contains title, artist, publisher_number, and full_json of the
+        original response.
+        """
+        output_dict_list = []
+        for release in release_list:
+            title = release["title"]
+            publisher_number = release["labels"][0]["catno"]
+            artist = ", ".join([artist["name"] for artist in release["artists"]])
+
+            release_dict = {
+                "title": title,
+                "artist": artist,
+                "publisher_number": publisher_number,
+                "full_json": release,
+            }
+
+            output_dict_list.append(release_dict)
+        return output_dict_list
--- tests/test_search_discogs.py
+++ tests/test_search_discogs.py
@@ -1,4 +1,4 @@
-from search_discogs import parse_discogs_data
+from searchers.discogs import DiscogsClient
 import json
 import unittest
 
@@ -7,12 +7,14 @@ class TestSearchDiscogs(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
+        # Create client for testing; fake values, only for initialization
+        cls.discogs_client = DiscogsClient(user_token="fake_token")
         # Load sample data for use by all tests
         with open("tests/sample_data/discogs_samples.data") as f:
             cls.data = json.load(f)
 
     def test_parse_discogs_data_one_result(self):
-        result = parse_discogs_data([self.data])
+        result = self.discogs_client.parse_discogs_data([self.data])
 
         self.assertEqual(len(result), 1)
         self.assertEqual(result[0]["title"], "Lincoln")
@@ -22,5 +24,5 @@ class TestSearchDiscogs(unittest.TestCase):
 
     def test_parse_discogs_data_no_result(self):
         # get_full_discogs_releases returns an empty list if no results
-        result = parse_discogs_data([])
+        result = self.discogs_client.parse_discogs_data([])
         self.assertEqual(result, [])
