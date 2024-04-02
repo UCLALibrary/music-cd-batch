537771f0b9b7aa14f67160262e58d332284a60f1
Zoe Tucker <zoetucker@library.ucla.edu>
2024-04-02 09:16:33 -0700

    add space character for 008/32

--- create_marc_record.py
+++ create_marc_record.py
@@ -36,7 +36,7 @@ def create_base_record() -> Record:
     # TrAr (008/33) - n Not arrangement or transposition or not specified
     # MRec (008/38) - # (Not modified)
     # Srce (008/39) - d (Other)
-    record.add_field(Field(tag="008", data="YYMMDDs||||    xx ||nn          n ||| d"))
+    record.add_field(Field(tag="008", data="YYMMDDs||||    xx ||nn           n ||| d"))
 
     # 040 ## $a CLU $b eng $c CLU
     # add_subfield method doesn't appear to work with newly created fields,
