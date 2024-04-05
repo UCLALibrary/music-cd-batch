cdae255ac45ed348f843845f5131014894d09711
Andy Kohler <akohler@library.ucla.edu>
2024-04-04 16:48:08 -0800

    Add tests for barcode and call number in local fields

--- tests/test_marc_creation.py
+++ tests/test_marc_creation.py
@@ -1,5 +1,7 @@
 import unittest
-from create_marc_record import create_base_record
+
+from pymarc import Subfield
+from create_marc_record import add_local_fields, create_base_record
 
 
 class TestBaseRecord(unittest.TestCase):
@@ -19,3 +21,28 @@ class TestBaseRecord(unittest.TestCase):
     def test_fld344_repeated(self):
         fld344s = self.base_record.get_fields("344")
         self.assertEqual(len(fld344s), 2)
+
+
+class TestLocalFields(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # Create simulated record for use in all tests in this class.
+        base_record = create_base_record()
+        cls.record = add_local_fields(
+            base_record, barcode="FAKE BARCODE", call_number="FAKE CALL NUMBER"
+        )
+
+    def test_barcode_is_added(self):
+        fld049 = self.record.get("049")
+        expected_subfields = [
+            Subfield(code="a", value="CLUV"),
+            Subfield(code="l", value="FAKE BARCODE"),
+        ]
+        self.assertEqual(fld049.subfields, expected_subfields)
+
+    def test_call_number_is_added(self):
+        fld049 = self.record.get("099")
+        expected_subfields = [
+            Subfield(code="a", value="FAKE CALL NUMBER"),
+        ]
+        self.assertEqual(fld049.subfields, expected_subfields)
